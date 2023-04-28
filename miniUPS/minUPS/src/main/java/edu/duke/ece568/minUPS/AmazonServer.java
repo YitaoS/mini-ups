66843f58e2439867c103e231b0f8655e3b8e68a5
ys386 <ys386@duke.edu>
2023-04-28 09:58:11 -0400

    d

--- miniUPS/minUPS/src/main/java/edu/duke/ece568/minUPS/AmazonServer.java
+++ miniUPS/minUPS/src/main/java/edu/duke/ece568/minUPS/AmazonServer.java
@@ -8,28 +8,28 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class AmazonServer {
     private final static Logger LOG =  LoggerFactory.getLogger(AmazonServer.class);
     private final ConnectionStream worldStream;
     private final ConnectionStream upsStream;
-    private final String WORLD_HOST = "vcm-33355.vm.duke.edu";
+    private final String WORLD_HOST = "vcm-33348.vm.duke.edu";
     private final int WORLD_PORT = 23456;
     private final String UPS_HOST = "vcm-33355.vm.duke.edu";
     private final int UPS_PORT = 22222;
 
-    @Autowired
-    public AmazonServer(Socket worldSocket,UpsServer upsServer) {
+   // @Autowired
+    public AmazonServer(Socket worldSocket,UpsServer upsServer) throws IOException{
         this.worldStream = new ConnectionStream(new Socket(WORLD_HOST, WORLD_PORT));
         this.upsStream = new ConnectionStream(new Socket(UPS_HOST, UPS_PORT));
         startServer();
     }
     private void startServer() {
-        try {
+        // try {
 
-        } catch (IOException e) {
-            LOG.error("Error starting server: " + e.getMessage());
-        }
+        // } catch (IOException e) {
+        //     LOG.error("Error starting server: " + e.getMessage());
+        // }
     }
 
 }
